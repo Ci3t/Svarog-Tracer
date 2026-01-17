25abc85d59b3481bbcb0f1ba06fba26ad564ce1f
Ci3t <ranigfx@gmail.com>
2026-01-17 20:28:03 +0200

    Fix: Replaced Broken Wiki URLs with stable HoyoLab URLs for Fate LCs

--- src/utils/warpDataService.js
+++ src/utils/warpDataService.js
@@ -218,7 +218,7 @@ export const FATE_LIGHT_CONES = [
   {
     id: "6001",
     name: "A Thankless Coronation",
-    image: "https://static.wikia.nocookie.net/honkai-star-rail/images/5/52/Light_Cone_A_Thankless_Coronation_Artwork.png",
+    image: "https://upload-os-bbs.hoyolab.com/upload/2025/07/11/1015613/5a5452f36f6e5c54df3a39e6d97e2f5b_7212450849223708226.png",
     type: "light_cone",
     separator: true,
     collaboration: "Fate/Stay Night"
@@ -226,7 +226,7 @@ export const FATE_LIGHT_CONES = [
   {
     id: "6002",
     name: "The Hell Where Ideals Burn",
-    image: "https://static.wikia.nocookie.net/honkai-star-rail/images/c/c8/Light_Cone_The_Hell_Where_Ideals_Burn_Artwork.png",
+    image: "https://upload-os-bbs.hoyolab.com/upload/2025/07/11/1015613/1e174786762c62c3d5262c5b7617b80a_431952520627253509.png",
     type: "light_cone",
     collaboration: "Fate/Stay Night"
   }
